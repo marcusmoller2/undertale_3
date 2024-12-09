ace1b9ba98aee81e2d6aeba561f80f2222de3981
marcus.mp3 <marcus.moller@elev.ga.ntig.se>
2024-12-09 10:52:58 +0100

    Create backstoryroom.py

---
+++ backstoryroom.py
@@ -0,0 +1,35 @@
+import time
+import playsound
+    
+    
+print("du går in i rummet och det verkar vara en tom koridor")
+time.sleep(2)
+print("du ser vad som ser ut som en liten gul blomma i mitten av koridoren")
+playsound.playsound('Undertale OST_ 001 - Once Upon A Time.mp3')
+print("blomman?: hejsan dig har jag inte sätt förut")
+time.sleep(3)
+print("blomman vänder sig om och du ser ett ansikte i blommans mitt")
+time.sleep(3)
+print("blomman?: se inte så förvånad ut har du aldrig sätt en blomma förut?")
+time.sleep(3)
+print("flowey: jag heter flowey blomman flowey och jag bor här")
+time.sleep(3)
+print("flowey: vad heter du?")
+floweyanswer = input()
+print(f"flowey: men hej på dig {floweyanswer} ska jag lära dig hur saker går till här?")
+tutorial = input()
+print("flowey: kom bara ihåg i den här världen är det döda eller bli dödad ")
+if tutorial.lower == "ja" or "y" or "yes":
+    print("flowey: perfekt ska vi bärja då")
+else:
+    print("too bad du får en tutorial ändå")
+print("flowey: först får du plocka up ett vapen")
+print("du plockade up en plast kniv")
+print("flowey: jag kommer kasta pellets på dig du ska undvika dom")
+
+print("flowey: du är nu redo för den här världen")
+print("flowey: kom ihåg i den här världen är det döda eller bli dödad")
+print("du står i korridoren en öppning brevid dig")
+print("du går genom öppningen")
+
+
